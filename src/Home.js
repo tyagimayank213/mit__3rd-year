ce66da9bf5b58078af10cc488d25f5b2cbf3680f
Mayank <tyagimayank213@gmail.com>
2021-11-02 09:58:56 +0530

    videos

--- src/Home.js
+++ src/Home.js
@@ -12,9 +12,13 @@ function Home(){
     console.log(auth);
     auth.signOut();
   }
+  function playVideo(file) {
+    setFileName(file);
+  }
+  const [fileName, setFileName] = useState('https://www.youtube.com/embed/wiuLCUZAWy8');
   return (
     <div>
-    <header id="home" style={{backgroundImage:'url(https://college-1be40.web.app/images/header-background.jpg)'}}>
+    <header id="home" style={{backgroundImage:'url(http://mitpharmacy.org/images/Header3.jpg)'}}>
       <div className="row banner">
         <div className="banner-text">
           <button className="signout" onClick={signOutt}>sign out</button>
@@ -86,10 +90,73 @@ function Home(){
       </div>
     </section>
     
-    <section id="portfolio">
+    <section id="portfolio1">
       <div className="row">
         <iframe src="https://mitmeerut.ac.in/" height="500" width="100%" title="Iframe Example"></iframe>
       </div>
+    </section>
+    <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Videos</h1>
+
+          <div className="videorow">
+            <div className="videocol">
+                <div className="feature-img">
+                    <img alt="" src="https://i.ytimg.com/vi/9i0FZJpby7M/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC7jJPB5GI-MyJ8u5U6F0Yh1OAqLg" width="100%" />
+                    <img alt="" src="https://www.freeiconspng.com/thumbs/button-icon-png/play-button-icon-png-17.png" className="play-btn" onClick={(e)=>playVideo('https://www.youtube.com/embed/9i0FZJpby7M')} />
+                </div>
+            </div>
+            <div className="videocol">
+                <div className="feature-img">
+                    <img alt="" src="https://i.ytimg.com/an_webp/wiuLCUZAWy8/mqdefault_6s.webp?du=3000&sqp=CJfsgIwG&rs=AOn4CLALE3T-aUROyaXfCHdLr-427_6Fng" width="100%" />
+                    <img alt="" src="https://www.freeiconspng.com/thumbs/button-icon-png/play-button-icon-png-17.png" className="play-btn" onClick={(e)=>playVideo('https://www.youtube.com/embed/wiuLCUZAWy8')}/>
+                </div>
+            </div>
+            <div className="videocol">
+                <div className="feature-img">
+                    <img alt="" src="https://i.ytimg.com/vi/q62SvxH2Vuk/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBGtJQlwbMdV2aI_LTYEzy96xrNGQ" width="100%" />
+                    <img alt="" src="https://www.freeiconspng.com/thumbs/button-icon-png/play-button-icon-png-17.png" className="play-btn" onClick={(e)=>playVideo('https://www.youtube.com/embed/q62SvxH2Vuk')}/>
+                </div>
+            </div>
+            <div className="videocol">
+                <div className="feature-img">
+                    <img alt="" src="https://i.ytimg.com/an_webp/haO2Lp4AGNk/mqdefault_6s.webp?du=3000&sqp=CKbUgIwG&rs=AOn4CLAEe0Y5Et4CF6rVhx33X_vf04e9WA" width="100%" />
+                    <img alt="" src="https://www.freeiconspng.com/thumbs/button-icon-png/play-button-icon-png-17.png" className="play-btn" onClick={(e)=>playVideo('https://www.youtube.com/embed/haO2Lp4AGNk')} />
+                </div>
+            </div>
+          </div>
+          <div className="videorow">
+            <div className="videocol">
+                <div className="feature-img">
+                    <img alt="" src="https://i.ytimg.com/vi/g1iPYSGqHiI/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB2ofgs-m4KXOx_JpXhTaYmLsHpug" width="100%" />
+                    <img alt="" src="https://www.freeiconspng.com/thumbs/button-icon-png/play-button-icon-png-17.png" className="play-btn" onClick={(e)=>playVideo('https://www.youtube.com/embed/g1iPYSGqHiI')} />
+                </div>
+            </div>
+            <div className="videocol">
+                <div className="feature-img">
+                    <img alt="" src="https://i.ytimg.com/vi/5F33ceKXXPA/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDsC_n8FoQ7kxCBvf5lv2xoGpvyEQ" width="100%" />
+                    <img alt="" src="https://www.freeiconspng.com/thumbs/button-icon-png/play-button-icon-png-17.png" className="play-btn" onClick={(e)=>playVideo('https://www.youtube.com/embed/5F33ceKXXPA')}/>
+                </div>
+            </div>
+            <div className="videocol">
+                <div className="feature-img">
+                    <img alt="" src="https://i.ytimg.com/an_webp/gWuDIsnwTkg/mqdefault_6s.webp?du=3000&sqp=CMD1gIwG&rs=AOn4CLCaRFlYXp2BUCwvkpC4QLIESTlg4A" width="100%" />
+                    <img alt="" src="https://www.freeiconspng.com/thumbs/button-icon-png/play-button-icon-png-17.png" className="play-btn" onClick={(e)=>playVideo('https://www.youtube.com/embed/gWuDIsnwTkg')}/>
+                </div>
+            </div>
+            <div className="videocol">
+                <div className="feature-img">
+                    <img alt="" src="https://i.ytimg.com/an_webp/gJ-gS2EHwts/mqdefault_6s.webp?du=3000&sqp=CLbpgIwG&rs=AOn4CLAOaIURybggpptJxEmDvZNbUKVC4g" width="100%" />
+                    <img alt="" src="https://www.freeiconspng.com/thumbs/button-icon-png/play-button-icon-png-17.png" className="play-btn" onClick={(e)=>playVideo('https://www.youtube.com/embed/gJ-gS2EHwts')}/>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="video-player">
+      <iframe width="1145" height="450" src={fileName} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
     </section>
     <footer>
       <div className="row">
